b84b518bab4219721de97da3414efdac59831f5f
amorozov <amorozov@accesssoftek.com>
2026-01-28 13:03:18 +0300

    update TagIds

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ afterEvaluate {
 
                 groupId = "com.github.arthur-morosov"
                 artifactId = "axiom-sdk-api"
-                version = "0.0.10"
+                version = "0.0.11"
             }
         }
     }
