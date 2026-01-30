ffeb21f551ca445bad7938a91746156d0ca6fc0b
amorozov <amorozov@accesssoftek.com>
2026-01-30 07:12:52 +0300

    fix model

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ afterEvaluate {
 
                 groupId = "com.github.arthur-morosov"
                 artifactId = "axiom-sdk-api"
-                version = "0.0.15"
+                version = "0.0.16"
             }
         }
     }
